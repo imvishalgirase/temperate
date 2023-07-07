e17550faaebc5836b9c74a4b1c225198ef4ad22e
imvishalgirase <137286597+imvishalgirase@users.noreply.github.com>
2023-07-06 23:39:50 -0700

    Add files via upload

---
+++ PR3PR7.C
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a=0,b=1,n,v,i;
+   clrscr();
+   printf("enter the value=");
+   scanf("%d",&n);
+   printf("%d,%d,",a,b);
+   for(i=2;i<=n;i++)
+   {
+      v=a+b;
+      a=b;
+      b=v;
+      printf("%d,",v);
+      }
+   getch();
+   }
